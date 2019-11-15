50d3af9e84710abb15faa8e847062b9962000cef
ruggedy <shout2john@yahoo.com>
2019-11-15 01:34:11 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ mod day_2;
 
 
 fn main() -> Result<()> {
-    // println!("============ DAY ONE ============");
-    // let day_one_frequency = day_1::calc_frequency()?;
-    // println!("Final frequency value {}", day_one_frequency);
+    println!("============ DAY ONE ============");
+    let day_one_frequency = day_1::calc_frequency()?;
+    println!("Final frequency value {}", day_one_frequency);
 
 
     println!("============ DAY TWO ============");
-    let day_two_multi_frequency = day_2::calc_multiple_frequency()?;
-    println!("Final frequency value {}", day_two_multi_frequency);
+    let day_one_multi_frequency = day_1::calc_multiple_frequency()?;
+    println!("Final frequency value {}", day_one_multi_frequency);
     Ok(())
 }
