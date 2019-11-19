80dd2423b78e1b0c82ae312470980e50ea871be5
ruggedy <shout2john@yahoo.com>
2019-11-19 15:35:58 +0000

    day 3 complete

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -1,51 +1,137 @@
 // use std::cmp::Ordering;
-use std::collections::{HashMap};
+use std::cmp::{self, Ordering, PartialOrd};
+use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufReader, Result as IoResult};
 
-#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
+#[derive(PartialEq, PartialOrd, Eq, Ord, Clone, Debug, Hash, Default)]
 struct Edge(u16, u16);
 
-type Weight = u16;
-
-pub fn calc_overlapping_materials() -> IoResult<u32> {
-    let f = File::open("files/day_three_input.txt")?;
-    let f = BufReader::new(f);
-    let values_hash_map =
-        f.lines()
-            .into_iter()
-            .fold(HashMap::new(), |mut acc: HashMap<Edge, Weight>, v| {
-                let values = v.unwrap();
-
-                let mut values = values
-                    .split(|c| c == ',' || c == ':' || c == 'x' || c == '@' || c == '#')
-                    .filter_map(|val| match val.trim().parse::<u16>() {
-                        Ok(v) => Some(v),
-                        _ => None,
-                    });
-
-                values.next().unwrap();
-                let x = values.next().unwrap();
-                let y = values.next().unwrap();
-                let width = values.next().unwrap();
-                let height = values.next().unwrap();
-
-
-                for pos in 0..width * height {
-                    let key = Edge(x + (pos % width), y + (pos / width));
-                    *acc.entry(key).or_insert(0) += 1;
-                }
-
-                acc
-            });
-
-    Ok(values_hash_map
-        .iter()
-        .fold(0, |acc, (_, value)| if *value > 1 { acc + 1 } else { acc }))
+#[derive(Eq, PartialOrd, Clone, Hash, Debug, Default)]
+struct Claim {
+    id: u16,
+    width: u16,
+    height: u16,
+    starting_point: Edge,
 }
 
-pub fn find_valid_claim() -> IoResult<String> {
+impl PartialEq for Claim {
+    fn eq(&self, other: &Self) -> bool {
+        self.starting_point == other.starting_point
+    }
+}
 
-    Ok("".to_string())
+impl Ord for Claim {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (
+            self.starting_point.0 + self.width,
+            self.starting_point.1 + self.height,
+        )
+            .cmp(
+                &((
+                    other.starting_point.0 + other.width,
+                    other.starting_point.1 + other.height,
+                )),
+            )
+    }
+}
+
+pub fn calc_overlapping_materials() -> IoResult<()> {
+    let f = File::open("files/day_three_input.txt")?;
+    let f = BufReader::new(f);
+    let mut claim_hash_set: BTreeSet<Claim> = BTreeSet::new();
+    let mut max_grid_length = 0;
+    for line in f.lines().into_iter() {
+        let values = line.unwrap();
+
+        let mut values = values
+            .split(|c| c == ',' || c == ':' || c == 'x' || c == '@' || c == '#')
+            .filter_map(|val| match val.trim().parse::<u16>() {
+                Ok(v) => Some(v),
+                _ => None,
+            });
+
+        let id = values.next().unwrap();
+        let x = values.next().unwrap();
+        let y = values.next().unwrap();
+        let width = values.next().unwrap();
+        let height = values.next().unwrap();
+
+        let claim = Claim {
+            id,
+            starting_point: Edge(x, y),
+            width,
+            height,
+        };
+
+        max_grid_length = cmp::max(max_grid_length, cmp::max(x + width, y + height));
+        claim_hash_set.insert(claim);
+    }
+
+    let mut grid: Vec<Vec<Vec<&Claim>>> =
+        vec![vec![vec![]; max_grid_length as usize]; max_grid_length as usize];
+
+    let mut potential_correct_claim: Vec<Claim> = Vec::new();
+    let mut count = 0;
+
+    let mut peekable_claim_iter = claim_hash_set.iter().peekable();
+
+    while let Some(claim) = peekable_claim_iter.next() {
+        let mut should_peek = true;
+
+        for y in (claim.starting_point.1) as usize..(claim.starting_point.1 + claim.height) as usize
+        {
+            for x in
+                (claim.starting_point.0) as usize..(claim.starting_point.0 + claim.width) as usize
+            {
+                if let Some(row) = grid.get_mut(y) {
+                    if let Some(column) = row.get_mut(x) {
+                        column.push(claim);
+
+                        let len = column.len();
+
+                        if len == 2 {
+                            count += 1;
+                        }
+
+                        if len >= 2 {
+                            should_peek = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        if should_peek {
+            if let Some(next_claim) = peekable_claim_iter.peek() {
+                let overlap = claim.starting_point.0 + claim.width > next_claim.starting_point.1
+                    && claim.starting_point.1 + claim.height > next_claim.starting_point.1;
+                if !overlap {
+                    potential_correct_claim.push(claim.clone());
+                }
+            }
+        }
+    }
+
+    println!("overlapping square inches {}", count);
+
+    'outer: for pot_claim in potential_correct_claim.iter() {
+        for y in (pot_claim.starting_point.1) as usize
+            ..(pot_claim.starting_point.1 + pot_claim.height) as usize
+        {
+            for x in (pot_claim.starting_point.0) as usize
+                ..(pot_claim.starting_point.0 + pot_claim.width) as usize
+            {
+                if grid[y][x].len() > 1 {
+                    continue 'outer;
+                }
+            }
+        }
+
+        println!("this is correct claim id {} ", pot_claim.id);
+        break;
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,16 @@ fn main() {
 
     println!("============ DAY ONE PART 2 ============");
     println!(
-        "First double frequency value {:?}",
-        day_1::calc_multiple_frequency()
+        "First double frequency value {}",
+        day_1::calc_multiple_frequency().unwrap()
     );
 
     println!("============ DAY TWO PART 1 ============");
-    println!("checksum value {:?}", day_2::calc_checksum());
+    println!("checksum value {}", day_2::calc_checksum().unwrap());
 
     println!("============ DAY TWO PART 2 ============");
-    println!("checksum value {:?}", day_2::find_boxes());
+    println!("checksum value {}", day_2::find_boxes().unwrap());
 
-    println!("============ DAY THREE PART 1 ============");
-    println!(
-        "intersection value {:?}",
-        day_3::calc_overlapping_materials()
-    );
-
-    println!("============ DAY THREE PART 2 ============");
-    println!("checksum value {:?}", day_3::find_valid_claim());
+    println!("============ DAY THREE  ============");
+    day_3::calc_overlapping_materials();
 }
